d8be6d6e652bdd73ad736ca69fd8a686f69e543a
Szczenathemonke <mpilkowski.learn@gmail.com>
2022-02-24 10:56:44 +0100

    task06_ver1
    for first feedback

--- tasks/06_layout/scripts/scripts.js
+++ tasks/06_layout/scripts/scripts.js
@@ -47,3 +47,19 @@ function myFunction() {
          y.style.display = "none";
         }
  }
+ function dropcat5(){
+    let y = document.getElementById("drop-item2-cat5");
+        if (y.style.display === "none") {
+        y.style.display = "flex";
+        } else {
+         y.style.display = "none";
+        }
+ }
+ function dropsort(){
+    let y = document.getElementById("drop-item2-sort");
+        if (y.style.display === "none") {
+        y.style.display = "flex";
+        } else {
+         y.style.display = "none";
+        }
+ }
